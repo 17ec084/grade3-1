c149515930ff7361e66098effd61b8c8c2da7e44
Tomotaka_Hirata <17ec084@ms.dendai.ac.jp>
2019-04-03 11:40:39 +0900

    update

---
+++ HirataExPressReiwa/index.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+if(isset($_POST['licenseKey']) && isLicensed($key=$_POST['licenseKey']))
+{
+//クライアントがライセンス認証されている場合
+    $userName=getUserName($key);//ユーザ名
+    $contents=getAvailableContents($key);//そのユーザが利用可能な問題集(の番号)を配列として取得
+
+}
+else
+{
+//クライアントがライセンス認証を受けていない場合
+    askLogin();//ログインを促し
+    $userName="guest";
+    $contents=getAvailableContents("guest");//ゲスト用のコンテンツを用意する
+}
+
+print dumpContents($key,$contents);
+
+
--- licenseContract/lcrp/readme.md
+++ licenseContract/lcrp/readme.md
@@ -1,5 +1,5 @@
-# lcrpサーバ
+﻿# lcrpサーバ
 
 - example0001: lcrpについて、[説明](https://github.com/17ec084/grade3-1/tree/master/licenseContract) するために作ったもの  
 
-- HirataExPressPremium: lcrpを利用して、[HirataExPress](https://github.com/17ec084/grade3-1/tree/master/HirataExPressPremium)の単語情報を有償にしたもの。
+- HirataExPressReiwa: lcrpを利用して、[HirataExPress](https://github.com/17ec084/grade3-1/tree/master/HirataExPressReiwa)の単語情報を有償にしたもの。
