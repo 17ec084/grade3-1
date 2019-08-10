3261078ecae200ce30c549a6a479797cf0c5c0d0
Tomotaka_Hirata <17ec084@ms.dendai.ac.jp>
2019-08-10 15:43:22 +0900

    Update DBG.java
    未完成だが、DBG命令をソースから取り除く機能や、DBGのクラス名を変更(厳密にはDBGを継承した子クラスの生成)する機能を追加。

--- groupStudy/statistical/src/myDebugger/DBG.java
+++ groupStudy/statistical/src/myDebugger/DBG.java
@@ -1,5 +1,11 @@
 package myDebugger;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class DBG
 {
 
@@ -20,61 +26,61 @@ public class DBG
 	{
 		indenter();
 		Object[] mAA = {"see",o};
-		privateSee(mAA);
+		protectedSee(mAA);
 	}
 
 	public static void see()
 	{
 		indenter();
 		Object[] mAA = {"see"};
-		privateSee(mAA);
+		protectedSee(mAA);
 	}
 
 	public static void seeln(Object o)
 	{
 		indenter();
 		Object[] mAA = {"seeln",o};
-		privateSee(mAA);
+		protectedSee(mAA);
 	}
 
 	public static void seeln()
 	{
 		indenter();
 		Object[] mAA = {"seeln"};
-		privateSee(mAA);
+		protectedSee(mAA);
 	}
 
 	public static void s(Object o)
 	{
 		indenter();
 		Object[] mAA = {"s",o};
-		privateSee(mAA);
+		protectedSee(mAA);
 	}
 
 	public static void s()
 	{
 		indenter();
 		Object[] mAA = {"s"};
-		privateSee(mAA);
+		protectedSee(mAA);
 	}
 
 	public static void sln(Object o)
 	{
 		indenter();
 		Object[] mAA = {"sln",o};
-		privateSee(mAA);
+		protectedSee(mAA);
 	}
 
 	public static void sln()
 	{
 		indenter();
 		Object[] mAA = {"sln"};
-		privateSee(mAA);
+		protectedSee(mAA);
 	}
 
-		private static void privateSee(Object[] methodAndArg)
+		protected static void protectedSee(Object[] methodAndArg)
 		{
-			StackTraceElement ste = Thread.currentThread().getStackTrace()[3];
+			StackTraceElement ste = Thread.currentThread().getStackTrace()[extendsLevel+3];
 			String cname = ste.getClassName();
 			String mname = ste.getMethodName();
 			String methodName = (String)methodAndArg[0];
@@ -120,13 +126,13 @@ public class DBG
 		{
 			indenter();
 			Object[] mAA = {"ln", condSymbol+" is true."};
-			privateSee(mAA);
+			protectedSee(mAA);
 		}
 		else
 		{
 			indenter();
 			Object[] mAA = {"ln", condSymbol+" is false."};
-			privateSee(mAA);
+			protectedSee(mAA);
 		}
 		return "";
 	}
@@ -137,13 +143,13 @@ public class DBG
 		{
 			indenter();
 			Object[] mAA = {"ln", condSymbol+" is true."};
-			privateSee(mAA);
+			protectedSee(mAA);
 		}
 		else
 		{
 			indenter();
 			Object[] mAA = {"ln", condSymbol+" is false."};
-			privateSee(mAA);
+			protectedSee(mAA);
 		}
 		return "";
 	}
@@ -169,13 +175,13 @@ public class DBG
 		{
 			indenter();
 			Object[] mAA = {"ln", whenTrue};
-				privateSee(mAA);
+				protectedSee(mAA);
 		}
 		else
 		{
 			indenter();
 			Object[] mAA = {"ln", whenFalse};
-				privateSee(mAA);
+				protectedSee(mAA);
 		}
 		return "";
 	}
@@ -186,13 +192,13 @@ public class DBG
 		{
 			indenter();
 			Object[] mAA = {"ln", whenTrue};
-				privateSee(mAA);
+				protectedSee(mAA);
 		}
 		else
 		{
 			indenter();
 			Object[] mAA = {"ln", whenFalse};
-				privateSee(mAA);
+				protectedSee(mAA);
 		}
 		return "";
 	}
@@ -231,7 +237,7 @@ public class DBG
 		{
 			indenter();
 			Object[] mAA = {"ln", "\""+ loopName + "\" has been looped "+ (cnt+1) + " times."};
-			privateSee(mAA);
+			protectedSee(mAA);
 		}
 		cnt++;
 		return "";
@@ -255,7 +261,7 @@ public class DBG
 		{
 			indenter();
 			Object[] mAA = {"ln", "\""+ loopName + "\" has been looped "+ (cnt+1) + " times."};
-			privateSee(mAA);
+			protectedSee(mAA);
 		}
 		return "";
 	}
@@ -271,7 +277,7 @@ public class DBG
 	}
 
 	//コンソールへの表示にインデントを付ける
-	private static int i = 0;
+	protected static int i = 0;
 
 	public static void goRight()
 	{
@@ -289,7 +295,7 @@ public class DBG
 		writeI(indent);
 	}
 
-		private static void writeI(int j)
+		protected static void writeI(int j)
 		{
 			/*
 			try
@@ -322,14 +328,14 @@ public class DBG
 		// */return i;
 	}
 
-	private static void indenter()
+	protected static void indenter()
 	{
 		int i = readI();
 		for(int j=0; j < i; j++)
 			System.out.print(" ");
 	}
 
-	private static String indenter(Object o)
+	protected static String indenter(Object o)
 	{
 		int i = readI();
 		boolean endWithReturn = false;
@@ -384,4 +390,182 @@ public class DBG
 		sleep(millisec, true);
 	}
 
+	//DBGの撤去
+	/**
+	 * 「/*DBG」と「DBG*&#047;」で囲まれたところ、そして
+	 * DBGのメソッド呼び出しをソースコードから取り除く。<br>
+	 * またDBGのメソッド呼び出しを含むif文、for文、[do-]while文、switch文、try-((catch)|(finally))文、メソッド宣言
+	 * については、コンソール上でユーザに確認しながら取り除く。<br>
+	 * (この機能を使うためにはブロックの省略をしてはならない)<br>
+	 * ※javaファイルはクラス名.javaというファイルをプロジェクトに対応するディレクトリから順番に探索することで特定する。<br>
+	 * したがって、別パッケージの同名クラスのjavaファイルに対して処理を行ってしまうことがある。<br>
+	 * これを防ぐためには、同名クラスのjavaファイルに「//DBG don't kill me」から始まる行を追加すること。
+	 *
+	 */
+	public static void killDBG()
+	{
+		File f = new File(findJavaFile());
+
+		String source = readSource(f);
+		//javaファイルを全行読み込む
+System.out.println(source);
+
+	}
+
+		protected static String findJavaFile()
+		{
+			File f = new File("").getAbsoluteFile();
+			while(!(f.getName().equals("statistical")))
+			{
+				f = f.getParentFile();
+			}
+			String javaFileName = Thread.currentThread().getStackTrace()[extendsLevel+3].getClassName();
+			//例:memoryHack.Main
+			javaFileName = javaFileName.replaceAll(".*\\.", "");
+			//例:Main
+			javaFileName += ".java";
+			//例:Main.java
+			return search(f, javaFileName).replaceAll("\\\\_hirata_breakRecursion", "");
+		}
+
+			protected static String search(File dir, String javaFileName)
+			{
+				File[] childlen = dir.listFiles();
+				String rtn;
+		        for(int i=0; i<childlen.length; i++)
+		        {
+		        	String split = "( |\t|\r|\n|(\r\n))*";
+		        	String split_must = "( |\\t|\\r|\\n|(\\r\\n))+";
+		        	if
+		        	(
+		        			childlen[i].isDirectory()
+		        		&&	has(childlen[i], javaFileName)
+		        		&&	hasRegex(childlen[i].getPath() + javaFileName, "import" + ".*" + "(myDebugger\\.)?" + myClassName + ".*" + ";")
+		        		&&	!hasRegex(childlen[i].getPath() + javaFileName, "//DBG" +split_must+ "((don\\'t)|(do"+split_must+"not))" +split_must+ "kill" +split_must+ "me")
+		        	)
+		        		return childlen[i].getPath() + "\\"+javaFileName+"\\_hirata_breakRecursion";
+		        	else
+		        	if(childlen[i].isDirectory())
+		                if((rtn=search(childlen[i], javaFileName)).matches(".*_hirata_breakRecursion"))
+		                	return rtn;
+		        }
+		        return javaFileName + " (which ) not found";
+		    }
+
+				protected static boolean has(File dir, String child)
+				{
+					File[] files = dir.listFiles();
+					for(int i=0; i < files.length; i++)
+						if(files[i].getName().equals(child))
+							return true;
+					return false;
+				}
+
+				protected static boolean hasRegex(String fileName, String regex)
+				{
+					return (readSource(new File(fileName)).matches(".*("+regex+").*"));
+				}
+
+		/**
+		 * 参考:https://qiita.com/penguinshunya/items/353bb1c555f337b0cf6d
+		 * @param f
+		 * @return
+		 */
+		protected static String readSource(File f)
+		{
+		    StringBuilder sb = new StringBuilder();
+
+		    try
+		    {
+		    	FileReader fr = new FileReader(f);
+		    	BufferedReader br = new BufferedReader(fr);
+
+		        String str = br.readLine();
+		        while (str != null)
+		        {
+		            sb.append(str + System.getProperty("line.separator"));
+		            str = br.readLine();
+		        }
+			    return sb.toString();
+		    }
+		    catch (IOException e){e.printStackTrace();}
+
+		    return null;
+
+
+		}
+
+		//DBGクラスのリネーム(他のデバッガとのクラス名前空間の衝突を回避)
+
+		//継承により実現させるから、see系では注意が必要。そこでextendsLevelというフィールドを導入する。
+
+		protected static int extendsLevel = 0;
+
+		protected static String myClassName = getDBGClassName();
+
+		protected static void setDBGClassName(String dbgClassName)
+		{
+			try
+			{
+				FileWriter fw;
+				fw = new FileWriter (new File("_hirata_dbgClassName"));
+				fw.write(dbgClassName);
+				fw.close();
+
+				File f = new File("").getAbsoluteFile();
+				while(!(f.getName().equals("statistical")))
+				{
+					f = f.getParentFile();
+				}
+				f = new File(search(f, "DBG.java").replaceAll("DBG\\.java", myClassName+".java").replaceAll("\\\\_hirata_breakRecursion", ""));
+				if(!f.exists())
+					for(int i=0; !f.createNewFile(); i++)
+						if(i>100)
+						{
+							cln("DBG couldn't be renamed to " + dbgClassName + ".java");
+							break;
+						}
+				for(int i=0; !f.setWritable(true); i++)
+					if(i>100)
+					{
+						cln(dbgClassName + ".java couldn't be writable");
+						break;
+					}
+				FileWriter fr = new FileWriter(f);
+				fr.write
+				(
+"package myDebugger;"+ 													"\r\n" +
+"" + 																	"\r\n" +
+"public class " + myClassName + " extends DBG" +						"\r\n" +
+"{" +																	"\r\n" +
+"	protected static int extendsLevel = 1;" +							"\r\n" +
+"	public " + myClassName + "(String loopName){super(loopName);}" +	"\r\n" +
+"}"
+						);
+				fr.close();
+				cln(dbgClassName + ".java is created in the directory of DBG.java .\n move it if needed and compile it.");
+			}
+			catch (IOException e){e.printStackTrace();}
+		}
+
+		protected static String getDBGClassName()
+		{
+			try
+			{
+				FileReader fr;
+				fr = new FileReader(new File("_hirata_dbgClassName"));
+				BufferedReader br = new BufferedReader(fr);
+				String rtn = br.readLine();
+				fr.close();
+				return rtn;
+			}
+			catch (IOException e){e.printStackTrace();}
+			return null;
+
+		}
+
+		public static void renameDBGto(String newName)
+		{
+			setDBGClassName(newName);
+		}
 }
